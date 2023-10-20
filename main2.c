1a6a2d7206c46ca2aef83ce32e07c1d1c6883769
Joao Pedro <jaauhp@gmail.com>
2023-10-20 14:26:20 -0300

    update final

--- main2.c
+++ main2.c
@@ -1,8 +1,8 @@
-int main(int argc, char *argv[]) {
-	
-	int n;
+int main(int argc, char *argv[])
+{
 
-    
+    int n;
+
     printf("---------------------------------------------------------\n");
     printf("Ola, Bem Vindo ao Simulador de Novos Salarios da Empresa X\n");
     printf("---------------------------------------------------------\n");
@@ -17,76 +17,64 @@ int main(int argc, char *argv[]) {
     scanf("%d", &n);
     printf("---------------------------------------------------------\n");
 
-   switch (n)
-   {
-   case 1:
-   printf("Voce Selecionou: Ajudante de Obras (Aumento Salarial de 9%%)\n");
-    break;
-   
-   case 2:
-   printf("Voce Selecionou: Encarregado de Obras (Aumento Salarial de 11%%)\n");
-   break;
+    switch (n)
+    {
+    case 1:
+        printf("Voce Selecionou: Ajudante de Obras (Aumento Salarial de 9%%)\n");
+        break;
 
-   case 3:
-   printf("Voce Selecionou: Mestre de Obras (Aumento Salarial de 14%%)\n");
-   break;
+    case 2:
+        printf("Voce Selecionou: Encarregado de Obras (Aumento Salarial de 11%%)\n");
+        break;
 
-   case 4:
-   printf("Voce Selecionou: Engenheiro Civil (Aumento Salarial de 14%%)\n");
-   break;
+    case 3:
+        printf("Voce Selecionou: Mestre de Obras (Aumento Salarial de 14%%)\n");
+        break;
 
-   case 5:
-   printf("Voce Selecionou: Engenheiro Mestre (Aumento Salarial de 16%%)\n");
-   break;
+    case 4:
+        printf("Voce Selecionou: Engenheiro Civil (Aumento Salarial de 14%%)\n");
+        break;
 
-   case 6:
-   printf("Voce Selecionou: Gerente de Projetos (Aumento Salarial de 18%%)\n");
-   break;
+    case 5:
+        printf("Voce Selecionou: Engenheiro Mestre (Aumento Salarial de 16%%)\n");
+        break;
 
-   default :
-  printf("Vaga Inavalida\n");
-    break;
-   }
-   
-    
+    case 6:
+        printf("Voce Selecionou: Gerente de Projetos (Aumento Salarial de 18%%)\n");
+        break;
 
-   float sal ,porcen ,aumento, novo_salario;
-   
-   printf("\n");
-   printf("Agora Informe Seu ATUAL Salario:  (Ultilize Apenas um Ponto entre as casas. Ex: 1.000) \n");
-   scanf("%f", &sal);
-   
-   printf("\n");
-   
-   printf("Informe a Portentagem de seu Aumento: (Sem o Simbolo de %% , Somente o numero. Ex: 14 para aumentos de 14%%)\n");
-   scanf("%f",&porcen);
-   
-   
-   aumento = sal * porcen/100;
-   novo_salario = sal + aumento;
-   
-   printf("\n");
-   printf("---------------------------------------------------------\n");
-   printf("\n");
-   printf("Seu Salario Ira Para: %.3f.\n", novo_salario);
+    default:
+        printf("Vaga Inavalida\n");
+        break;
+    }
 
- 
-  
-   
+    float sal, porcen, aumento, novo_salario;
+
+    printf("\n");
+    printf("Agora Informe Seu ATUAL Salario:  (Ultilize Apenas um Ponto entre as casas. Ex: 1.000) \n");
+    scanf("%f", &sal);
+
+    printf("\n");
+
+    printf("Informe a Portentagem de seu Aumento: (Sem o Simbolo de %% , Somente o numero. Ex: 14 para aumentos de 14%%)\n");
+    scanf("%f", &porcen);
+
+    aumento = sal * porcen / 100;
+    novo_salario = sal + aumento;
+
+    printf("\n");
+    printf("---------------------------------------------------------\n");
+    printf("\n");
+    printf("Seu Salario Ira Para: %.3f.\n", novo_salario);
+
+    printf("\n");
+    printf("\n");
+    printf("Obrigado Por Testar Meu Projeto De Faculdade (:\n");
+    printf("Feito Por Joao Pedro - 2 Periodo De Engenharia de Software\n");
+
+    printf("Aperte Qualquer Tecla Para Sair\n");
 
     system("pause");
-    
-    
-    
-    
-    
+
     return 0;
 }
-    
-    
-    
-    
-    
-    
-    
-    
